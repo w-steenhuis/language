e8bd733f76b34c3422dadf0185186180c5f5a45a
wieger.steenhuis <wieger.steenhuis@dynalinq.eu>
2019-04-12 15:20:32 +0200

    add tranlation json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'language';
+  data: any;
+
+  constructor(private _http: HttpClient) {
+    this.data = this._http.get('http://www.anwb.nl/feeds/gethf');
+  }
+
+
+
 }
