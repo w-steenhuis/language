1733496714540719800b502232a47ff61cf2e315
wieger.steenhuis <wieger.steenhuis@dynalinq.eu>
2019-04-16 09:03:36 +0200

    finilize poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,20 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'language';
-  languages: string[] = ['nl', 'en', 'fr'];
+  selected: string;
+  languages: string[] = ['nl', 'en', 'fr', 'de'];
 
   constructor(private translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
+    this.selected = 'nl';
     this.translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    this.translate.use('nl');
+    this.translate.use(this.selected );
   }
 
   switchLanguage(lang: string) {
+    this.selected = lang;
     this.translate.use(lang);
   }
 }
--- src/app/lazymodule/lazy.component.ts
+++ src/app/lazymodule/lazy.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   template: `
-    <h1>{{ 'LAZY_LOADING_TITLE' | translate }}</h1>
+    <h1>{{ "LAZY_LOADING_TITLE" | translate }}</h1>
+    <img src="https://www.gstatic.com/webp/gallery3/2.png" />
+    <div class="balloon"
+    >
+      <h5 style="display: inline-block; margin: 50px;">{{ "THANKS" | translate }}</h5>
+    </div>
   `
 })
 export class LazyComponent {}
